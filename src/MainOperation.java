8de00a33abb631a3648c5becc57d45fab1b12b0f
Aleksei Pirogov <alekseipirogovQA@yandex.ru>
2026-01-13 17:56:13 +0300

    Add Operation

---
+++ src/MainOperation.java
@@ -0,0 +1,5 @@
+public class MainOperation {
+    public static void main(String[] args) {
+        System.out.println(Operation.ADD.apply(1, 2));
+    }
+}
---
+++ src/Operation.java
@@ -0,0 +1,28 @@
+public enum Operation {
+    ADD {
+        @Override
+        public int apply(int x, int y) {
+            return x + y;
+        }
+    },
+    SUBTRACT {
+        @Override
+        public int apply(int x, int y) {
+            return x - y;
+        }
+    },
+    MULTIPLY {
+        @Override
+        public int apply(int x, int y) {
+            return x * y;
+        }
+    },
+    DIVIDE {
+        @Override
+        public int apply(int x, int y) {
+            return x / y;
+        }
+    };
+
+    public abstract int apply(int x, int y);
+}
